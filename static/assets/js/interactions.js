05759ac5d0f79e93162a4bfb3058939ab8fe35c1
Jonathan Balsano <jonathan.balsano@10gen.com>
2017-02-10 16:10:47 -0500

    Wire a bunch of stuff together

--- static/assets/js/interactions.js
+++ static/assets/js/interactions.js
@@ -1,25 +1,38 @@
 $(document).ready(function() {
-	$('#search').focus();
-	$('.activate-group').click(function() {
-		$('.tabs-header li').removeClass('active');
-		$('.tabs-group li').hide();
-		$('.details-tab').addClass('active');
-		$('.details-panel').show();
-		$('.ad-details').slideUp();
-		$('.result-group').removeClass('dropshadow');
-		$(this).parent().addClass('dropshadow');
-		$(this).siblings('.ad-details').slideDown();
-	});
-	$('.tabs-header li').click(function() {
-		$('.tabs-header li').removeClass('active');
-		$(this).addClass('active');
-	});
-	$('.details-tab').click(function() {
-		$('.tabs-group li').hide();
-		$('.details-panel').show()
-	});
-	$('.involved-tab').click(function() {
-		$('.tabs-group li').hide();
-		$('.involved-panel').show()
-	});
+    $.get('/sponsors').then(function(results) {
+        const $templateResult = $('#template-result');
+        results.forEach(function(result) {
+            const $resultElement = $templateResult.clone();
+            const { host, urls, paths } = result;
+            $resultElement.attr('hidden', false);
+            $resultElement.find('.hostname-container').text(host);
+            $resultElement.find('.url-container').text(urls[0]);
+            $resultElement.find('.path-count-container').text(paths.length);
+            $resultElement.find('.hostname-link').attr('href', host);
+            $('#all-results').append($resultElement);
+        });
+        $('#search').focus();
+        $('.activate-group').click(function() {
+            $('.tabs-header li').removeClass('active');
+            $('.tabs-group li').hide();
+            $('.details-tab').addClass('active');
+            $('.details-panel').show();
+            $('.ad-details').slideUp();
+            $('.result-group').removeClass('dropshadow');
+            $(this).parent().addClass('dropshadow');
+            $(this).siblings('.ad-details').slideDown();
+        });
+        $('.tabs-header li').click(function() {
+            $('.tabs-header li').removeClass('active');
+            $(this).addClass('active');
+        });
+        $('.details-tab').click(function() {
+            $('.tabs-group li').hide();
+            $('.details-panel').show()
+        });
+        $('.involved-tab').click(function() {
+            $('.tabs-group li').hide();
+            $('.involved-panel').show()
+        });
+    });
 })
